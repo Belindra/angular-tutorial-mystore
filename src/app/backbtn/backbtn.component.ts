9d663a49666ceea26be9fab001966ee176117c6e
Sirene <s2000583@edu.bc.fi>
2021-01-25 12:53:48 +0200

    end of tutorial

--- src/app/backbtn/backbtn.component.ts
+++ src/app/backbtn/backbtn.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faLongArrowAltLeft } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-backbtn',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./backbtn.component.css']
 })
 export class BackbtnComponent implements OnInit {
+  faLongArrowAltLeft = faLongArrowAltLeft;
 
   constructor() { }
 
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -9,11 +9,13 @@ import { products } from '../products';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
+  products = products;
   product;
 
   constructor(private route: ActivatedRoute, private cartService: CartService) { }
 
   ngOnInit(): void {
+    console.log(this.products);
     this.route.paramMap.subscribe(params => {
       this.product = products[+params.get('productId')];
  });
